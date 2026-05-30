9a03cbc096f5fbe0c538d8ccec5501085c148584
Slowpool <nevznachay02@gmail.com>
2026-05-30 12:54:24 +0300

    module-info.java fix to prev commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,9 @@
 module org.swetlokognatsk {
     requires javafx.controls;
-    requires javafx.fxml;
     requires javafx.media;
     requires org.apache.commons.lang3;
 
-    opens org.swetlokognatsk.earking_out to javafx.fxml;
-
+    // TODO why to specify these exports?
     exports org.swetlokognatsk.earking_out;
     // TODO remove in prod
     exports org.swetlokognatsk.test;
